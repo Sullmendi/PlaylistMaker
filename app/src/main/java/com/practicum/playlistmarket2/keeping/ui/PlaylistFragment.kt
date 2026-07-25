215b83fa4132c8b5604aba18016e51cd175fbc92
Sveta <svetictv@mail.ru>
2026-07-25 19:10:43 +0300

    Домашнее задание Спринт 18

---
+++ app/src/main/java/com/practicum/playlistmarket2/keeping/ui/PlaylistFragment.kt
@@ -0,0 +1,44 @@
+package com.practicum.playlistmarket2.keeping.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.R
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmarket2.databinding.PlaylistsFragmentBinding
+import com.practicum.playlistmarket2.keeping.domain.api.PlaylistState
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.getValue
+
+class PlaylistFragment: Fragment() {
+    private var _binding: PlaylistsFragmentBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PlaylistViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = PlaylistsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.observeState().observe(this){
+            render(it)
+        }
+    }
+    fun render(state: PlaylistState){
+        binding.emptyPlaylistText.text = getString(com.practicum.playlistmarket2.R.string.empty_media_playlists)
+        binding.emptyPlaylistImage.setImageResource(com.practicum.playlistmarket2.R.drawable.ic_not_found_120)
+    }
+
+    companion object{
+        fun newInstance(): PlaylistFragment{
+            return PlaylistFragment().apply {  }
+        }
+    }
+}
---
+++ app/src/main/java/com/practicum/playlistmarket2/keeping/ui/PlaylistViewModel.kt
@@ -0,0 +1,17 @@
+package com.practicum.playlistmarket2.keeping.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.practicum.playlistmarket2.keeping.domain.api.PlaylistState
+
+class PlaylistViewModel: ViewModel() {
+    private val stateLiveData = MutableLiveData<PlaylistState>()
+    fun observeState(): LiveData<PlaylistState> = stateLiveData
+    init {
+        renderState(PlaylistState.Empty)
+    }
+    fun renderState(state: PlaylistState){
+        stateLiveData.postValue(state)
+    }
+}
