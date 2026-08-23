90e3fa83feafcbf554037755fa2e96d15aeaacc7
Sveta <svetictv@mail.ru>
2026-08-23 12:27:30 +0300

    Домашнее задание Спринт 20

--- app/src/main/java/com/practicum/playlistmarket2/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/practicum/playlistmarket2/data/network/RetrofitNetworkClient.kt
@@ -1,31 +1,22 @@
 package com.practicum.playlistmarket2.data.network
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import com.practicum.playlistmarket2.data.CheckNetworkConnection
-import com.practicum.playlistmarket2.data.network.NetworkClient
 import com.practicum.playlistmarket2.data.dto.Response
-import com.practicum.playlistmarket2.search.data.network.TrackSearchRequest
 import com.practicum.playlistmarket2.search.data.network.TrackItunesApi
+import com.practicum.playlistmarket2.search.data.network.TrackSearchRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import okhttp3.Dispatcher
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitNetworkClient(private val checkConnection: CheckNetworkConnection, private val trackService: TrackItunesApi) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
         if (dto is TrackSearchRequest) {
             if (checkConnection.isConnected()) {
-                return withContext(Dispatchers.IO) {
-                    try {
+                return try {
                         val resp = trackService.findTrack(dto.searchText)
                         resp.apply { resultCode = 200 }
                     } catch (e: Throwable) {
                         Response().apply { resultCode = -2 }
                     }
-                }
             } else {
                 return Response().apply { resultCode = -1 }
             }
